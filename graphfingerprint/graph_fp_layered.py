0ed71c6ea6697923466a5ef7900a807394602c4d
Eric Ma <ericmajinglong@gmail.com>
2016-04-04 17:18:04 -0400

    pep8 fixes

--- graphfingerprint/graph_fp_layered.py
+++ graphfingerprint/graph_fp_layered.py
@@ -4,7 +4,8 @@ import numpy as np
 
 from sklearn.preprocessing import LabelBinarizer
 from random import sample, choice
-from convnet import GraphInputLayer, GraphConvLayer, FingerprintLayer, LinearRegressionLayer
+from convnet import GraphInputLayer, GraphConvLayer, FingerprintLayer,\
+    LinearRegressionLayer
 from wb2 import WeightsAndBiases
 from flatten import flatten
 from time import time
@@ -70,19 +71,23 @@ print('Score of the graph:')
 print(score(G))
 
 n_nodes = [i for i in range(2, len(all_nodes))]
-n_graphs = 200
-graphs = [make_random_graph(sample(all_nodes, choice(n_nodes)), choice(n_nodes), features_dict) for i in range(n_graphs)]
-len(graphs)
+n_graphs = 10
+
+# Make all synthetic graphs
+graphs = [make_random_graph(sample(all_nodes, choice(n_nodes)), 
+                            choice(n_nodes), 
+                            features_dict) for i in range(n_graphs)]
 
 input_shape = (1, 10)
 inputs = GraphInputLayer(input_shape).forward_pass(graphs)
-node_indices, nbr_indices, graph_indices = GraphInputLayer(input_shape).graph_indices(graphs)
-print('Node indices:')
-print(node_indices)
-print('Neighbor indices:')
-print(nbr_indices)
-print('Graph indices:')
-print(graph_indices)
+node_indices, nbr_indices, graph_indices = \
+    GraphInputLayer(input_shape).graph_indices(graphs)
+# print('Node indices:')
+# print(node_indices)
+# print('Neighbor indices:')
+# print(nbr_indices)
+# print('Graph indices:')
+# print(graph_indices)
 
 layers = [GraphConvLayer(kernel_shape=(10, 20)),
           GraphConvLayer(kernel_shape=(20, 20)),
@@ -90,7 +95,7 @@ layers = [GraphConvLayer(kernel_shape=(10, 20)),
           # GraphConvLayer(kernel_shape=(10, 10)),
           FingerprintLayer(10),
           LinearRegressionLayer(shape=(10, 1))]
-
+print(layers)
 
 def initialize_network(input_shape, layers, graphs):
     """
@@ -114,7 +119,8 @@ def initialize_network(input_shape, layers, graphs):
 wb_all = initialize_network(input_shape, layers, graphs)
 
 
-def predict(wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_indices, graph_indices):
+def predict(wb_vect, wb_unflattener, inputs, layers, node_indices,
+            nbr_indices, graph_indices):
     curr_inputs = inputs
     start = time()
     wb_all = wb_unflattener(wb_vect)
@@ -122,34 +128,44 @@ def predict(wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_indices, 
     # print('Time to unflatten: {0}'.format(end - start))
     for i, layer in enumerate(layers):
         wb = wb_all['layer{0}_{1}'.format(i, layer)]
-        curr_inputs = layer.forward_pass(wb, curr_inputs, node_indices, nbr_indices, graph_indices)
+        curr_inputs = layer.forward_pass(wb, curr_inputs, node_indices,
+                                         nbr_indices, graph_indices)
     return curr_inputs
 
-predict(*wb_all.flattened(), inputs, layers, node_indices, nbr_indices, graph_indices)
+predict(*wb_all.flattened(), inputs, layers, node_indices, nbr_indices,
+        graph_indices)
 
 graph_scores = np.array([float(score(g)) for g in graphs])
-print(graph_scores.dtype)
+print(type(graph_scores))
 
-def train_loss(wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_indices, graph_indices, graphs, graph_scores):
+def train_loss(wb_vect, wb_unflattener, inputs, layers, node_indices,
+               nbr_indices, graph_indices, graphs, graph_scores):
     """
     Training loss is MSE.
     """
-    preds = predict(wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_indices, graph_indices)
+    preds = predict(wb_vect, wb_unflattener, inputs, layers, node_indices,
+                    nbr_indices, graph_indices)
     # actual = np.array([score(g) for g in graphs])
 
     mse = np.sum(np.power(preds - graph_scores, 2)) / len(graphs)
     # mse = np.sum(np.abs(preds - graph_scores)) / len(graphs)
     return mse
 
-# train_loss(*wb_all.flattened(), inputs, layers, node_indices, nbr_indices, graph_indices, graphs, graph_scores)
+# Test the train_loss function
+# train_loss(*wb_all.flattened(), inputs, layers, node_indices, nbr_indices,
+#            graph_indices, graphs, graph_scores)
 
 gradfunc = grad(train_loss)
-# gradfunc(*wb_all.flattened(), inputs, layers, node_indices, nbr_indices, graph_indices, graphs, graph_scores)
+
+# Test the gradfunc function.
+# gradfunc(*wb_all.flattened(), inputs, layers, node_indices, nbr_indices,
+#          graph_indices, graphs, graph_scores)
 
 wb_vect, wb_unflattener = wb_all.flattened()
 
 
-def sgd(gradfunc, wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_indices, graph_indices, graphs, graph_scores, callback=None, num_iters=200, step_size=0.1, mass=0.9):
+def sgd(gradfunc, wb_vect, wb_unflattener, inputs, layers, node_indices,
+        nbr_indices, graph_indices, graphs, graph_scores, callback=None, num_iters=200, step_size=0.1, mass=0.9):
     """
     Stochastic gradient descent with momentum.
     """
@@ -159,17 +175,19 @@ def sgd(gradfunc, wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_ind
         start = time()
         print('Epoch: {0}'.format(i))
         print('Computing gradient w.r.t. weights...')
-        g = gradfunc(wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_indices, graph_indices, graphs, graph_scores)
+        g = gradfunc(wb_vect, wb_unflattener, inputs, layers, node_indices,
+                     nbr_indices, graph_indices, graphs, graph_scores)
         velocity = mass * velocity - (1.0 - mass) * g
         wb_vect += step_size * velocity
         print('Training Loss: ')
-        print(train_loss(wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_indices, graph_indices, graphs, graph_scores))
+        print(train_loss(wb_vect, wb_unflattener, inputs, layers,
+                         node_indices, nbr_indices, graph_indices, graphs,
+                         graph_scores))
         end = time()
         print('Time: {0}'.format(end - start))
         print('')
     # return wb_vect, wb_unflattener
 
-sgd(gradfunc, wb_vect, wb_unflattener, inputs, layers, node_indices, nbr_indices, graph_indices, graphs, graph_scores, num_iters=1000, step_size=0.0001)
-
-# new_graphs = [make_random_graph(sample(all_nodes, choice(n_nodes)), choice(n_nodes), features_dict) for i in range(10)]
-
+sgd(gradfunc, wb_vect, wb_unflattener, inputs, layers, node_indices,
+    nbr_indices, graph_indices, graphs, graph_scores, num_iters=1000,
+    step_size=0.0001)

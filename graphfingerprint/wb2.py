18d442cad89b4ad014ef2b5ab71e5e94ad4e805f
Eric Ma <ericmajinglong@gmail.com>
2016-04-04 11:33:52 -0400

    Added 2nd version of wb for use with layers.

---
+++ graphfingerprint/wb2.py
@@ -0,0 +1,31 @@
+from flatten import flatten
+import autograd.numpy.random as npr
+
+
+class WeightsAndBiases(dict):
+    """
+    A class that stores the WeightsAndBiases for convolution.
+    """
+    def __init__(self):
+        # super(WeightsAndBiases, self).__init__()
+        # self.initialize_layers(n_layers, shapes)
+        # self.vect, self.unflattener = self.flattened()
+        pass
+
+    def flattened(self):
+        flattened_weights, unflattener = flatten(self)
+        self.vect = flattened_weights
+        self.unflattener = unflattener
+        return flattened_weights, unflattener
+
+    def add(self, name, shape):
+        """
+        Add a randomly initialized set of weights/biases to the WB class. It
+        is initialized with mean 0 and variance 0.1
+
+        Parameters:
+        ===========
+        - name: (string) self_weights, nbr_weights, biases, or some other name.
+        - shape: (tuple) the dimensions of the layer.
+        """
+        self[name] = npr.normal(0, 0.1, shape)

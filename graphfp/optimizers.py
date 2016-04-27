2e92d24984329767eb4891103ab440d416f31245
Eric Ma <ericmajinglong@gmail.com>
2016-04-27 01:33:25 -0400

    added adam optimizer

--- graphfp/optimizers.py
+++ graphfp/optimizers.py
@@ -2,7 +2,7 @@ from .flatten import flatten
 import autograd.numpy as np
 
 
-def sgd(gradfunc, wb, layers, graphs, callback=None, num_iters=200,
+def sgd(gradfunc, wb, callback=None, num_iters=200,
         step_size=0.1, mass=0.9, adaptive=False):
     """
     Batch stochastic gradient descent with momentum.
@@ -25,5 +25,28 @@ def sgd(gradfunc, wb, layers, graphs, callback=None, num_iters=200,
         wb = wb_unflattener(wb_vect)
         if callback:
             callback(wb, i)
+
+    return wb_vect, wb_unflattener
+
+
+def adam(grad, wb, callback=None, num_iters=100,
+         step_size=0.001, b1=0.9, b2=0.999, eps=10**-8):
+    """Adam as described in http://arxiv.org/pdf/1412.6980.pdf.
+    It's basically RMSprop with momentum and some correction terms."""
+
+    wb_vect, wb_unflattener = flatten(wb)
+
+    m = np.zeros(len(wb_vect))
+    v = np.zeros(len(wb_vect))
+
+    for i in range(num_iters):
+        g = grad(wb_vect, wb_unflattener)
+        if callback:
+            callback(wb_unflattener(wb_vect), i)
+        m = (1 - b1) * g + b1 * m  # First  moment estimate.
+        v = (1 - b2) * (g**2) + b2 * v  # Second moment estimate.
+        mhat = m / (1 - b1**(i + 1))    # Bias correction.
+        vhat = v / (1 - b2**(i + 1))
+        wb_vect -= step_size*mhat/(np.sqrt(vhat) + eps)
 
     return wb_vect, wb_unflattener

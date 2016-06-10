c0e976df8e6e305c80ca1467761580f37c23002d
Eric J. Ma <ericmajinglong@gmail.com>
2016-06-10 16:02:43 -0400

    Fixed import statements.

--- graphfp/test_binary_matrix_utils.py
+++ graphfp/test_binary_matrix_utils.py
@@ -1,7 +1,7 @@
-from binary_matrix_utils import (to_sparse_format,
-                                 sparse_binary_transpose,
-                                 binary_matrix_to_sparse_rows,
-                                 sparse_rows_to_binary_matrix)
+from .binary_matrix_utils import (to_sparse_format,
+                                  sparse_binary_transpose,
+                                  binary_matrix_to_sparse_rows,
+                                  sparse_rows_to_binary_matrix)
 import numpy as np
 
 dct = {0: [0, 1, 2],
